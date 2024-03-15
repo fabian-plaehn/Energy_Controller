cdd8e364cf315205192aac2706af781888665043
Fabian <fabian.plaehn@stud.uni-goettingen.de>
2024-03-15 10:07:03 +0100

    up2date

--- main.py
+++ main.py
@@ -136,6 +136,8 @@ def main():
             for stack in Mining_Stacks:
                 stack.set_sheet()
                 
+            # TODO check for invalid sheets when pc booted and send new one
+                
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
