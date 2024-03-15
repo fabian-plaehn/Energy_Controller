4400301cd45483629da3b97287e58567bb02d1be
Fabian <fabianplaehn28@gmail.com>
2024-03-15 10:15:59 +0100

    up2date

---
+++ sunny/sunny_test.py
@@ -0,0 +1,14 @@
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import time
+driver = webdriver.Firefox()
+
+driver.get("http://127.0.0.1:1880/ui/")
+
+while True:
+    print(driver.)  # bcharing_power
+    time.sleep(1)
+    
